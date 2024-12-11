3b1b09b8aed674f3e8072f376b11fb424dd879ec
Alex Ly <70249534+Alexlychee@users.noreply.github.com>
2024-12-10 18:09:50 -0800

    replaces IATA column with the word TESTING for now and formatted class functions/methods

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,16 @@
 import requests
 from pprint import pprint
 
+SHEETY_URL_ENDPOINT = "https://api.sheety.co/f83f5bdcf3c2f74a3d16e3a94425ffca/flightDeals/prices"
+
 class DataManager:
     # This class is responsible for talking to the Google Sheet.
-    response = requests.get(url="https://api.sheety.co/f83f5bdcf3c2f74a3d16e3a94425ffca/flightDeals/prices")
-    data = response.json()
-    sheet_data = data["prices"]
-    print(sheet_data)
+    def __init__(self):
+        self.sheet_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_URL_ENDPOINT)
+        data = response.json()
+        self.sheet_data = data["prices"]
+        return self.sheet_data
 
