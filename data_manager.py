01d8b20ea306676a69264736335468c3e0a745ff
Alex Ly <70249534+Alexlychee@users.noreply.github.com>
2024-12-10 17:33:41 -0800

    used Sheety API to GET data in the google sheets

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,10 @@
+import requests
+from pprint import pprint
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    # This class is responsible for talking to the Google Sheet.
+    response = requests.get(url="https://api.sheety.co/f83f5bdcf3c2f74a3d16e3a94425ffca/flightDeals/prices")
+    data = response.json()
+    sheet_data = data["prices"]
+    print(sheet_data)
+
